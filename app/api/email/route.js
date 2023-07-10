fdc1f40dcc601490e38890a43cbc84ac765d7d2f
Rajaekb <kobirajae@gmail.com>
2023-07-10 18:01:09 +0100

    edit contact api

---
+++ app/api/email/route.js
@@ -0,0 +1,29 @@
+import nodemailer from "nodemailer";
+
+export default async (req, res) => {
+  const { email } = req.body;
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  try {
+    await transporter.sendMail({
+      from: email,
+      to: process.env.EMAIL_USER,
+      subject: `Email Subcription  from ${email}`,
+      html: `<p>You have an email form Subcription form</p><br>
+        <p><strong>Email: </strong> ${email}</p><br>
+
+      `,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message || error.toString() });
+  }
+  return res.status(200).json({ error: "" });
+};
