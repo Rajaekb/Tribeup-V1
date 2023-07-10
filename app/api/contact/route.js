bddd70fe7ab3f53632e693ef3a821d7db760b9d0
Rajaekb <kobirajae@gmail.com>
2023-07-10 17:59:18 +0100

    edit contact api

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,11 +1,29 @@
-export default function handler(req, res) {
-  if (req.method === "POST") {
-    // Handle the POST request
-    console.log(req.body);
-    // Access request data using req.body
-    // Perform any necessary validation or processing
+import nodemailer from "nodemailer";
 
-    // Example response
-    res.status(200).json({ message: "Form submitted successfully" });
+export default async (req, res) => {
+  const { name, email, message } = req.body;
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  try {
+    await transporter.sendMail({
+      from: email,
+      to: process.env.EMAIL_USER,
+      subject: `Contact form submission from ${name}`,
+      html: `<p>You have a contact form submission</p><br>
+        <p><strong>Email: </strong> ${email}</p><br>
+        <p><strong>Message: </strong> ${message}</p><br>
+      `,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message || error.toString() });
   }
-}
+  return res.status(200).json({ error: "" });
+};
