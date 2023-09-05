63862954687c667947aa1acbf100d7ba873c623d
Rajaekb <kobirajae@gmail.com>
2023-09-05 16:43:44 +0100

    edit

--- app/[locale]/about/page.jsx
+++ app/[locale]/about/page.jsx
@@ -4,7 +4,7 @@ import { slideIn, staggerContainer, textVariant } from '../utils/motion';
 import { TypingText } from "../components";
 import Image from "next/image";
 import { useTranslations } from "next-intl";
-import sp1 from '../../public/sp1.png'
+import sp1 from '../../public/bureau2.jpg'
 
 
 
@@ -30,36 +30,34 @@ const About = () => {
                         variants={slideIn('left', 'tween', 0.8, 1)}
                         className="flex">
                         <div className='sm:mt-5  sm:w-60 w-20 '>
-                            <div className='border border-[#FEBF03] m-1 rounded-lg' />
+                            <div className='border border-[#d0ff05] m-1 rounded-lg' />
                         </div>
 
                         <TypingText title={t('aboutus')} />
                         <div className='sm:mt-5  sm:w-60 w-20 '>
-                            <div className='border border-[#FEBF03] m-1 rounded-lg' />
+                            <div className='border border-[#d0ff05] m-1 rounded-lg' />
                         </div>
                     </motion.div>
 
                 </div>
 
 
-                <div className="text-gray-300 sm:text-xl text-md tracking-[.05em] sm:leading-normal leading-relaxed pt-5 z-10 mx-64">
+                <div className="text-gray-300 sm:text-2xl text-md tracking-[.05em] sm:leading-normal leading-relaxed  z-10 mx-64">
+
+                    <h2 className="text-strock-gray text-5xl md:text-7xl uppercase py-5 sm:leading-normal leading-relaxed py-10">La Technologie Au Service De Votre Croissance</h2>
+                    <div className="bg-[#d0ff05] pt-5 pl-36 h-[800px] mb-24 w-[250px]">
+                        <Image
+                            src={sp1}
+                            width={1000}
+                            className="object-contain absolute my-10 "
 
 
-                    <p>{t('aboutustext')}</p><br />
-                    <p>{t('aboutusp1')}</p>
-                    <Image
-                        src={sp1}
-                        width={700}
-                        className="object-contain mx-auto my-10"
+                        />
 
-
-                    />
+                    </div>
 
                     <p>{t('aboutusp2')}</p><br />
-                    <p>{t('aboutusp3')}</p><br />
-                    <p>{t('aboutusp4')}</p><br />
-                    <p>{t('aboutusp5')}</p><br />
-                    <p>{t('aboutusp6')}</p><br />
+
                 </div>
 
             </motion.div>
--- app/[locale]/sections/About.jsx
+++ app/[locale]/sections/About.jsx
@@ -24,6 +24,7 @@ const About = () => {
         <motion.div
           variants={textVariant(0.3)}
         >
+
           <Image
             src={rug}
             alt="rug2"
