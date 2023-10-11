79f97c4fc83a45eb4acd33ba7b14536502dc1b2c
Rajaekb <kobirajae@gmail.com>
2023-10-11 10:18:10 +0100

    edit

--- app/[locale]/sections/GetStarted.jsx
+++ app/[locale]/sections/GetStarted.jsx
@@ -79,13 +79,13 @@ const GetStarted = () => {
                   <h1 className=" hidden sm:block text-8xl mt-24 ml-80 font-bold glassmorphism2 uppercase text-stone-900">
                     games</h1>
                 </motion.div>
-              </div>
-              <div className=' hidden sm:flex justify-end  h-6 mt-24 '>
+                <div className=' hidden sm:flex justify-end  h-6 mt-24  '>
 
-                <div className=' greenline uppercase z-50 '>
-                  {t('gamesdescp5')}
-                </div>
-              </div>
+                  <div className=' greenline uppercase z-50 '>
+                    {t('gamesdescp5')}
+                  </div>
+                </div> </div>
+
 
             </motion.div>
             <div className='flex justify-center  rounded-xl sm:w-[43%] rounded-xl sm:h-[500px]'
